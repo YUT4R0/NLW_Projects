86c9b276679212a59e999e9f8ff3887a612774b7
Yutaro <pedroyutaronakamura@gmail.com>
2023-10-10 17:21:41 -0400

    integrated project with h2 tadabase and jpa, also updated usermodel and created a IUserRepository that extedns jparepository, and updated UserController to make the create() function be a ResponseEntity type wich enables to use springframweworks to return a body and http message based on the user req

--- todolist/src/main/java/br/com/yutaro/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/yutaro/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.yutaro.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel usermodel) {
-    System.out.println(usermodel.name);
+  public ResponseEntity create(@RequestBody UserModel usermodel) {
+    var user = this.userRepository.findByUsername(usermodel.getUsername());
+
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This user already exists.");
+    }
+
+    var userCreated = this.userRepository.save(usermodel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
